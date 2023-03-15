e14f6c04155ccf503bcdd073663e60c37c80aa4e
gaomingqiang <846359845@qq.com>
2023-03-15 17:10:15 +0800

    123

---
+++ cloud-provider-payment-8001/src/main/java/com/dad/springcloud/dao/PaymentDao.java
@@ -0,0 +1,17 @@
+package com.dad.springcloud.dao;
+
+
+import com.dad.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.ArrayList;
+
+@Mapper
+public interface PaymentDao {
+    int createPayment(Payment payment);
+    int delPaymentById(@Param("id") Long id);
+    Payment getPaymentById(@Param("id") Long id );
+    ArrayList<Payment>  getPayMentList();
+    int upPayment(Payment payment);
+}
